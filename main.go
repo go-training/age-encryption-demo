7f038231b87f0021cc605c71655622863eac6052
appleboy <appleboy.tw@gmail.com>
2023-09-16 23:20:57 +0800

    chore: update variable name
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- main.go
+++ main.go
@@ -11,16 +11,17 @@ import (
 )
 
 func main() {
-	k, err := age.GenerateX25519Identity()
+	privateKey, err := age.GenerateX25519Identity()
 	if err != nil {
 		log.Fatalf("internal error: %v", err)
 	}
+	publicKey := privateKey.Recipient()
 
 	if !term.IsTerminal(int(os.Stdout.Fd())) {
-		fmt.Fprintf(os.Stderr, "Public key: %s\n", k.Recipient())
+		fmt.Fprintf(os.Stderr, "Public key: %s\n", publicKey)
 	}
 
 	fmt.Fprintf(os.Stdout, "# created: %s\n", time.Now().Format(time.RFC3339))
-	fmt.Fprintf(os.Stdout, "# public key: %s\n", k.Recipient())
-	fmt.Fprintf(os.Stdout, "%s\n", k)
+	fmt.Fprintf(os.Stdout, "# public key: %s\n", publicKey)
+	fmt.Fprintf(os.Stdout, "%s\n", privateKey)
 }
